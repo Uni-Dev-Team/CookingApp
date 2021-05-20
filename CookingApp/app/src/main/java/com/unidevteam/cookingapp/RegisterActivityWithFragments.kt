550b7d03e0b3ac87422c74a68e64bdf301f193c8
Borotalcohol <loschiavo.christian@gmail.com>
2021-05-20 14:23:40 +0200

    Implementazione funzionalità registrazione e pulizia file inutili

--- CookingApp/app/src/main/java/com/unidevteam/cookingapp/RegisterActivityWithFragments.kt
+++ CookingApp/app/src/main/java/com/unidevteam/cookingapp/RegisterActivityWithFragments.kt
@@ -10,10 +10,18 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 private const val NUM_PAGES = 2
 
 class RegisterActivityWithFragments : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     private lateinit var viewPager: ViewPager2
 
     private lateinit var _name : String
@@ -23,6 +31,8 @@ class RegisterActivityWithFragments : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_with_fragments)
+
+        auth = FirebaseAuth.getInstance()
 
         viewPager = findViewById(R.id.fragment_container_view_registration)
 
@@ -34,9 +44,8 @@ class RegisterActivityWithFragments : AppCompatActivity() {
                 val name = findViewById<TextView>(R.id.tf_name).text.toString()
                 val email = findViewById<TextView>(R.id.tf_email).text.toString()
                 // Da aggiungere:
-                // - Controlli con regex per formato email e nome
                 // - Log grafico per errori e avvisi all'utente
-                if(name.isNotEmpty() && email.isNotEmpty()) {
+                if(name.isNotEmpty() && email.isNotEmpty() && nameValidator(name) && emailValidator(email)) {
                     Log.d(TAG, "Nome: $name")
                     Log.d(TAG, "Email: $email")
                     viewPager.setCurrentItem(1)
@@ -48,25 +57,33 @@ class RegisterActivityWithFragments : AppCompatActivity() {
                 val password1 = findViewById<TextView>(R.id.tf_password_1).text.toString()
                 val password2 = findViewById<TextView>(R.id.tf_password_2).text.toString()
                 // Da aggiungere:
-                // - Controlli con regex sul formato delle password
                 // - Log grafico per errori e avvisi all'utente
-                if(password1.isNotEmpty() && password2.isNotEmpty() && password1.equals(password2)) {
+                if(password1.isNotEmpty() && password2.isNotEmpty() && password1.equals(password2) && passValidator(password1)) {
                     Log.d(TAG, "Password 1: $password1")
                     Log.d(TAG, "Password 2: $password2")
                     _password = password1
+
+                    if (auth.currentUser == null) {
+                        firebaseCreateUser(_email, _password)
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    Log.d(TAG, "Registration: User created correctly")
+                                    firebaseSendPasswordVerification(auth.currentUser)
+                                    auth.signOut()
+                                    gotoLoginPage()
+                                } else {
+                                    Log.d(TAG, "Registration: Failed - ${task.exception}")
+                                }
+                            }
+                    } else { Log.d(TAG, "Error: User already logged")
+                        gotoLoginPage()
+                    }
                 }
             }
         }
 
-        /*if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<RegisterStep1>(R.id.fragment_container_view_registration)
-            }
-        }*/
         findViewById<TextView>(R.id.lb_loginPage).setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            gotoLoginPage()
         }
     }
 
@@ -83,6 +100,57 @@ class RegisterActivityWithFragments : AppCompatActivity() {
         override fun getItemCount(): Int = NUM_PAGES
         override fun createFragment(position: Int): Fragment = if(position==0) RegisterStep1() else RegisterStep2()
     }
+
+    // [START goto_login_page]
+    private fun gotoLoginPage(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+    // [END goto_login_page]
+
+    // [START password_regex]
+    // TODO: pass validation is never used, bring this to register pt 2
+    private fun passValidator(text: String?):Boolean{
+        val pattern: Pattern = Pattern.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@\$ %^&*-]).{8,}\$")
+        val matcher: Matcher = pattern.matcher(text)
+        return matcher.matches()
+    }
+    // [END password_regex]
+
+    // [START email_regex]
+    private fun emailValidator(text: String?):Boolean{
+        val pattern: Pattern = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)\$")
+        val matcher: Matcher = pattern.matcher(text)
+        return matcher.matches()
+    }
+    // [END email_regex]
+
+    //[START] name_regex
+    private fun nameValidator(text: String?): Boolean {
+        val pattern: Pattern = Pattern.compile("^([a-zA-Z]{2,}\\s[a-zA-Z]{1,}'?-?[a-zA-Z]{1,}\\s?([a-zA-Z]{1,})?)")
+        val matcher: Matcher = pattern.matcher(text)
+        return matcher.matches()
+    }
+    //[END] name_regex
+
+    // [START create_user]
+    // TODO bring this to registration pt 2 because it's never used
+    private fun firebaseCreateUser(email: String, password: String) : Task<AuthResult> {
+        return auth.createUserWithEmailAndPassword( email, password)
+    }
+    // [END create_user]
+
+    // [START send_verification_email]
+    private fun firebaseSendPasswordVerification(user: FirebaseUser) {
+        user.sendEmailVerification().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.d(TAG, "Email: sent")
+            } else {
+                Log.d(TAG, "Email: Failed - ${task.exception}")
+            }
+        }
+    }
+    // [END send_verification_email]
 
     companion object {
         private const val TAG = "RegisterActivity"
