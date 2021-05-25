8f57ab870c30c960d7e720a927e597d71e981429
Borotalcohol <loschiavo.christian@gmail.com>
2021-05-25 17:01:58 +0200

    Aggiunta implementazione per CAIngredient.kt

--- CookingApp/app/src/main/java/com/unidevteam/cookingapp/models/CAIngredient.kt
+++ CookingApp/app/src/main/java/com/unidevteam/cookingapp/models/CAIngredient.kt
@@ -1,8 +1,22 @@
 package com.unidevteam.cookingapp.models
 
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.QuerySnapshot
+
 data class CAIngredient(val title: String, val category: String, val allergens: Array<String>) {
 
     fun toHashMap() : HashMap<String, Any> {
         return hashMapOf("title" to title, "category" to category, "allergens" to allergens)
+    }
+
+    companion object {
+        @Suppress("UNCHECKED_CAST")
+        fun fromData(data: Map<String, Any>) : CAIngredient {
+            return CAIngredient(
+                title = data["title"].toString(),
+                category = data["category"].toString(),
+                allergens = data["allergens"] as Array<String>
+            )
+        }
     }
 }
