9155c6eb126eedcf793b09ac2a5dc7fe80327f45
Borotalcohol <loschiavo.christian@gmail.com>
2021-06-28 17:06:16 +0200

    Inizio sviluppo grafica fragment aggiunta ricetta

--- CookingApp/app/src/main/java/com/unidevteam/cookingapp/activities/Home/AddRecipeFragment.kt
+++ CookingApp/app/src/main/java/com/unidevteam/cookingapp/activities/Home/AddRecipeFragment.kt
@@ -1,20 +1,64 @@
 package com.unidevteam.cookingapp.activities.Home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import com.unidevteam.cookingapp.R
 
 class AddRecipeFragment : Fragment() {
+    private lateinit var viewOfLayout: View
+
+    @SuppressLint("CutPasteId")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_add_recipe, container, false)
+    ): View? {
+        viewOfLayout = inflater.inflate(R.layout.fragment_add_recipe, container, false)
+
+        var timeItems : MutableList<String> = mutableListOf<String>()
+        (5..60 step 5).forEach {
+            timeItems.add(it.toString())
+        }
+
+        val difficultyItems = mutableListOf<String>("Facile", "Normale", "Difficile")
+        val costItems = mutableListOf<String>("Basso", "Medio", "Alto")
+
+        val timeListViewAdapter : ArrayAdapter<String> = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_dropdown_item,
+            timeItems
+        )
+
+        val difficultyListViewAdapter : ArrayAdapter<String> = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_dropdown_item,
+            difficultyItems
+        )
+
+        val costListViewAdapter : ArrayAdapter<String> = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_dropdown_item,
+            costItems
+        )
+
+        viewOfLayout.findViewById<Spinner>(R.id.recipeTimeSpinner).adapter = timeListViewAdapter
+        viewOfLayout.findViewById<Spinner>(R.id.recipeDifficultySpinner).adapter = difficultyListViewAdapter
+        viewOfLayout.findViewById<Spinner>(R.id.recipeCostSpinner).adapter = costListViewAdapter
+
+        return viewOfLayout
+    }
 
     // override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     //    super.onViewCreated(view, savedInstanceState)
     // }
+
+    companion object {
+        private const val TAG = "AddRecipeFragment"
+    }
 }
